b79509ccc979557885e59fcbe431dbf114e36241
HK <hkdevtools@gmail.com>
2016-12-02 15:23:31 +0000

    Initial project commit

---
+++ src/main/java/com/hkdev/HkdevApplication.java
@@ -0,0 +1,12 @@
+package com.hkdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkdevApplication.class, args);
+	}
+}
