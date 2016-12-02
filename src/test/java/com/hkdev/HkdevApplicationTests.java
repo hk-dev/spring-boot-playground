3c1222f1353d3ff7607310e11f55fa3cbe1a93a0
HK <hkdevtools@gmail.com>
2016-12-02 17:26:09 +0000

    Add i18n feature basics

--- src/test/java/com/hkdev/HkdevApplicationTests.java
+++ src/test/java/com/hkdev/HkdevApplicationTests.java
@@ -1,16 +1,26 @@
 package com.hkdev;
 
+import com.hkdev.web.i18n.I18NService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HkdevApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private I18NService i18NService;
 
+	@Test
+	public void testMessageByLocalService() throws Exception {
+		String expected = "Bootstrap starter template";
+		String messageId = "index.main.title";
+		String actual = i18NService.getMessage(messageId);
+		assertEquals(expected, actual);
+	}
 }
