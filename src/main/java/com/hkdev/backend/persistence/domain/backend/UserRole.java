e92655edb59d13f931183b8e332937221c073d0c
HK <hkdevtools@gmail.com>
2016-12-04 22:11:35 +0000

    Created JPA entity models

---
+++ src/main/java/com/hkdev/backend/persistence/domain/backend/UserRole.java
@@ -0,0 +1,58 @@
+package com.hkdev.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user_role")
+public class UserRole implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public UserRole() {
+    }
+
+    @Id
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private User role;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getRole() {
+        return role;
+    }
+
+    public void setRole(User role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRole userRole = (UserRole) o;
+
+        if (user != null ? !user.equals(userRole.user) : userRole.user != null) return false;
+        return role != null ? role.equals(userRole.role) : userRole.role == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = user != null ? user.hashCode() : 0;
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
+}
